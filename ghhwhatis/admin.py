0140af7bd04366a2f401c21d9d416c161c827ffd
Matt Mansour <slackbabbath@gmail.com>
2012-06-04 16:58:34 -0700

    Adding Diffpage model and admin

---
+++ ghhwhatis/admin.py
@@ -0,0 +1,26 @@
+from ghhwhatis.models import DifferncePage
+from django.contrib import admin
+from mezzanine.core.admin import DisplayableAdmin
+
+
+
+class DifferncePageAdmin(DisplayableAdmin):
+
+    fieldsets = [
+        ("Title",                       {'fields': ['title']}),
+        ("Published Date",              {'fields': ['publish_date']}),
+        ("Published Status",            {'fields': ['status']}),
+        ("Subject One",                 {'fields': ['subject_one']}),
+        ("Subject Two",                 {'fields': ['subject_two']}),
+        ("Subject One Data",            {'fields': ['subject_one_data']}),
+        ("Subject Two Data",            {'fields': ['subject_two_data']}),
+    ]
+
+    list_display = ('title', 'status', 'publish_date', 'subject_one', 'subject_two',)
+    list_editable = ('status',)
+    list_filter = ['status', 'publish_date',]
+    search_fields = ['title',]
+    date_hierarchy = 'publish_date'
+
+admin.site.register(DifferncePage, DifferncePageAdmin)
+
--- ghhwhatis/models.py
+++ ghhwhatis/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from mezzanine.core.models import Displayable, RichTextField
 # Create your models here.
+class DifferncePage(Displayable):
+    subject_one = models.CharField(max_length=400, verbose_name="First Subject", blank=True)
+    subject_two = models.CharField(max_length=400, verbose_name="Second Subject", blank=True)
+    subject_one_data = RichTextField(blank=True, verbose_name="Subject One Data")
+    subject_two_data = RichTextField(blank=True, verbose_name="Subject Two Data")
+
+    @models.permalink
+    def get_absolute_url(self):
+        return ('ghhwhatis.views.detail', [self.slug,])
+
+    def __unicode__(self):
+        return self.title
+
