2f24526174509c7fbfe349cde30fa3ba7ac6b497
Matt Mansour <slackbabbath@gmail.com>
2012-06-04 12:55:44 -0700

    Adding subdomains to the site

---
+++ ghhwhatis/views.py
@@ -0,0 +1,13 @@
+# Create your views here.
+from django.http import HttpResponse, Http404
+
+def what_is(request):
+    try:
+        # Retrieve the user account associated with the current subdomain.
+#        user = User.objects.get(username=request.subdomain)
+        return HttpResponse(request.subdomain)
+    except:
+#    except User.DoesNotExist:
+        # No user matches the current subdomain, so return a generic 404.
+        raise Http404
+
