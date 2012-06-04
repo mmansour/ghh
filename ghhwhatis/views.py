e25fe519b577fe9592450ef856bab69e1be48d3f
Matt Mansour <slackbabbath@gmail.com>
2012-06-04 13:39:08 -0700

    Printing subdomain

--- ghhwhatis/views.py
+++ ghhwhatis/views.py
@@ -5,6 +5,7 @@ def what_is(request):
     try:
         # Retrieve the user account associated with the current subdomain.
 #        user = User.objects.get(username=request.subdomain)
+        print request.subdomain
         return HttpResponse(request.subdomain)
     except:
 #    except User.DoesNotExist:
