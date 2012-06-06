24980829995207d7c937af3494eaeaca7d73421b
Matt Mansour <slackbabbath@gmail.com>
2012-06-05 17:42:34 -0700

    Adding API for OpenSearch

---
+++ ghhwhatis/data_apis.py
@@ -0,0 +1,33 @@
+############################################## BEAUTIFULSOUP
+import urllib2
+from bs4 import BeautifulSoup
+
+#SUBJECT 1
+def get_subject_one_data(data):
+    subject_one_dict = {}
+    subject_one_wiki_url = "http://en.wikipedia.org/w/api.php?action=opensearch&limit=1&namespace=0&format=xml&search={0}"\
+        .format(data)
+    req1 = urllib2.Request(subject_one_wiki_url)
+    resp1 = urllib2.urlopen(req1)
+    xml1 = resp1.read()
+    soup1 = BeautifulSoup(xml1)
+    subject_one_dict['query'] = soup1.query.text
+    subject_one_dict['description'] = soup1.description.text
+    return subject_one_dict
+
+
+#SUBJECT 2
+def get_subject_two_data(data):
+    subject_two_dict = {}
+    subject_two_wiki_url = "http://en.wikipedia.org/w/api.php?action=opensearch&limit=1&namespace=0&format=xml&search={0}"\
+        .format(data)
+    req2 = urllib2.Request(subject_two_wiki_url)
+    resp2 = urllib2.urlopen(req2)
+    xml2 = resp2.read()
+    soup2 = BeautifulSoup(xml2)
+    subject_two_dict['query'] = soup2.query.text
+    subject_two_dict['description'] = soup2.description.text
+    return subject_two_dict
+
+print get_subject_one_data('spinach')
+print get_subject_two_data('kale')
--- ghhwhatis/views.py
+++ ghhwhatis/views.py
@@ -4,7 +4,6 @@ from django.template import RequestContext
 from django.http import HttpResponse, Http404, HttpResponsePermanentRedirect
 
 def what_is(request):
-    
     return render_to_response('ghhwhatis/index.html',
                        {},
                         context_instance=RequestContext(request))
