fb99c466c2268255a7fe34a68255ee50af1de1dd
Matt Mansour <slackbabbath@gmail.com>
2012-06-08 12:54:46 -0700

    Creating explore by topic section

--- ghhwhatis/views.py
+++ ghhwhatis/views.py
@@ -1,5 +1,5 @@
 from ghhwhatis.models import DifferncePage
-from django.shortcuts import render_to_response, get_object_or_404
+from django.shortcuts import render_to_response, get_object_or_404, get_list_or_404
 from django.template import RequestContext
 from data_apis import *
 from django.http import HttpResponse, Http404, HttpResponsePermanentRedirect
@@ -8,5 +8,12 @@ def what_is(request, page_slug):
     diffdata = get_object_or_404(DifferncePage, slug=page_slug)
     return render_to_response('ghhwhatis/index.html',
                        {'diffdata':diffdata},
+                        context_instance=RequestContext(request))
+
+def explore_a_z(request, page_slug):
+    explore_alphebet_section = DifferncePage.objects.filter(slug__startswith=page_slug)
+    print explore_alphebet_section
+    return render_to_response('pages/explore-topics-list.html',
+                       {'explore_alphebet_section':explore_alphebet_section, 'page_slug':page_slug},
                         context_instance=RequestContext(request))
 
