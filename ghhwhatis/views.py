7fe538cd5083848c326262e665c01efbfd18fad6
Matt Mansour <slackbabbath@gmail.com>
2012-06-08 15:22:51 -0700

    Cleanup

--- ghhwhatis/views.py
+++ ghhwhatis/views.py
@@ -1,8 +1,7 @@
 from ghhwhatis.models import DifferncePage
 from django.shortcuts import render_to_response, get_object_or_404, get_list_or_404
 from django.template import RequestContext
-from data_apis import *
-from django.http import HttpResponse, Http404, HttpResponsePermanentRedirect
+#from django.http import HttpResponse, Http404, HttpResponsePermanentRedirect
 
 def what_is(request, page_slug):
     diffdata = get_object_or_404(DifferncePage, slug=page_slug)
@@ -12,7 +11,6 @@ def what_is(request, page_slug):
 
 def explore_a_z(request, page_slug):
     explore_alphebet_section = DifferncePage.objects.filter(slug__startswith=page_slug)
-    print explore_alphebet_section
     return render_to_response('pages/explore-topics-list.html',
                        {'explore_alphebet_section':explore_alphebet_section, 'page_slug':page_slug},
                         context_instance=RequestContext(request))
