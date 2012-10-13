1621bafa387426a199130e251d0ca81e775fb211
Matt Mansour <slackbabbath@gmail.com>
2012-10-12 22:17:35 -0700

    Fixing

--- ghhwhatis/urls.py
+++ ghhwhatis/urls.py
@@ -4,7 +4,7 @@ admin.autodiscover()
 
 from mezzanine.core.views import direct_to_template
 
-urlpatterns = patterns('ghhwhatis.views',
+urlpatterns = patterns('ghh.ghhwhatis.views',
     url("^$", "compare", name="compare"),
     url(r'^(?P<page_slug>[\w-]+)/$', 'what_is'),
 )
