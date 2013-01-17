1c62aef97f97705719ad30debf85a69b9cd52417
Matt Mansour <slackbabbath@gmail.com>
2013-01-16 20:19:19 -0800

    Test

--- ghhwhatis/defaults.py
+++ ghhwhatis/defaults.py
@@ -15,6 +15,12 @@ register_setting(
     description=_("Sequence of setting names available within templates."),
     editable=False,
     default=(
-        "SHOW_ADS",
+        "ACCOUNTS_ENABLED", "ADMIN_MEDIA_PREFIX", "SHOW_ADS",
+        "BLOG_BITLY_USER", "BLOG_BITLY_KEY",
+        "COMMENTS_DISQUS_SHORTNAME", "COMMENTS_NUM_LATEST",
+        "COMMENTS_DISQUS_API_PUBLIC_KEY", "COMMENTS_DISQUS_API_SECRET_KEY",
+        "DEV_SERVER", "FORMS_USE_HTML5", "GRAPPELLI_INSTALLED",
+        "GOOGLE_ANALYTICS_ID", "JQUERY_FILENAME", "LOGIN_URL", "LOGOUT_URL",
+        "PAGES_MENU_SHOW_ALL", "SITE_TITLE", "SITE_TAGLINE", "RATINGS_MAX",
     ),
 )
