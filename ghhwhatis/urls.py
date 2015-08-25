bd2f637414d6b8c1b4085ab8f25fcb55033bcf5e
Matt Mansour <slackbabbath@gmail.com>
2015-08-24 21:32:38 -0700

    fix

--- ghhwhatis/urls.py
+++ ghhwhatis/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls.defaults import *
 from django.contrib import admin
-from django.views.generic import TemplateView
 admin.autodiscover()
 
 from mezzanine.core.views import direct_to_template
@@ -8,8 +7,6 @@ from mezzanine.core.views import direct_to_template
 urlpatterns = patterns('ghh.ghhwhatis.views',
     url("^$", "compare", name="compare"),
     url(r'^(?P<page_slug>[\w-]+)/$', 'what_is'),
-    url(r'^flippa_5439457.txt', TemplateView.as_view(
-        template_name='generic/flippa.html')),
 )
 
 
