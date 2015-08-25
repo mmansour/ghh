dfdf82acb6045ea532ff59fc783d3fba0bf4ddb2
Matt Mansour <slackbabbath@gmail.com>
2015-08-24 21:22:03 -0700

    verify flippa

--- ghhwhatis/urls.py
+++ ghhwhatis/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import *
 from django.contrib import admin
+from django.views.generic import TemplateView
 admin.autodiscover()
 
 from mezzanine.core.views import direct_to_template
@@ -7,6 +8,8 @@ from mezzanine.core.views import direct_to_template
 urlpatterns = patterns('ghh.ghhwhatis.views',
     url("^$", "compare", name="compare"),
     url(r'^(?P<page_slug>[\w-]+)/$', 'what_is'),
+    url(r'^flippa_5439457.txt', TemplateView.as_view(
+        template_name='generic/flippa.html')),
 )
 
 
